54188d7b2917c7459692bde07ed99058ce75fecf
ckubec <ckubec@gmail.com>
2016-02-11 19:32:48 -0800

    Easter egg original Commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+/*
+ * TCSS 360
+ * Easter Egg
+ */
+
+public final class Main {
+
+    /**
+     *
+     */
+    public static void main(final String[] theArgs) {
+        //Display message with everyone's name
+        System.out.println("Hello this is the Easter Egg for our 360 project the B# Team: \n");
+        System.out.println("Chris Kubec,\n");
+    }
+}
