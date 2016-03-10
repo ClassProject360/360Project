bb4585fb45696f1bffda0c7dcf684dd24b146321
Shawn Massoud <shawnhm1@gmail.com>
2016-03-10 00:03:30 -0800

    Revert "Shawn Sub."
    This reverts commit 8bdabe882b39d3bacb70527a94fd0aa71786b640.

--- Main.java
+++ Main.java
@@ -12,6 +12,5 @@ public final class Main {
         //Display message with everyone's name
         System.out.println("Hello this is the Easter Egg for our 360 project the B# Team: \n");
         System.out.println("Chris Kubec,\n");
-	System.out.println("Shawn Massoud,\n");
     }
 }
