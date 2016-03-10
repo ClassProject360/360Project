ec0fc6ffd7eff23142b2538d408f17cb21a5fea2
Shawn Massoud <shawnhm1@gmail.com>
2016-03-10 00:08:51 -0800

    Update Judge.java

--- 360/src/panels/Judge.java
+++ 360/src/panels/Judge.java
@@ -1,23 +1,151 @@
-/*
- * 
- */
 package panels;
-
-//import java.awt.BorderLayout;
 import java.awt.Color;
-import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Enumeration;
 import javax.swing.JPanel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import javax.swing.JRadioButton;
+import javax.swing.ListSelectionModel;
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
 
-/**
- * 
- *
- */
 @SuppressWarnings("serial") //Not implementing serialization suppressing warning.
-public class Judge extends JPanel {
-
-	public Judge(int width, int height, JButton logout) {
-		setBackground(Color.PINK);
+public class JudgePanel extends JPanel 
+{
+	public JudgePanel(int width, int height) throws IOException 
+	{
+		setBackground(Color.BLUE);
+		JButton logoutButton = new JButton("Log Out");
+		logoutButton.setBounds(361, 24, 117, 37);
+		logoutButton.setHorizontalAlignment(SwingConstants.LEFT);
+		ButtonGroup buttonGroup = null;
+		DefaultListModel listModel;
 		
-		add(logout);
+		
+		logoutButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//getContentPane().removeAll();
+				//getContentPane().add(about(), BorderLayout.CENTER);
+				//getContentPane().add(buttonPanel, BorderLayout.NORTH);
+				//printAll(getGraphics());
+			}
+		});
+		setLayout(null);
+		
+		FileReader file = new FileReader("C:\\Users\\shawn\\workspace\\360Project-master"
+				+ "\\360\\src\\project\\infor.txt");
+        BufferedReader reader = new BufferedReader(file);
+        
+        //counts how many lines we have which determinants number if contestants
+        int numOfLines = (int) reader.lines().count();
+
+        listModel = new DefaultListModel();
+     
+        for(int i = 0; i < numOfLines; i ++)
+        {
+            listModel.addElement(i+1);
+        }
+        
+		JList list = new JList(listModel);
+		
+		list.setBounds(22, 140, 215, 350);
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		add(list);
+		
+		//CODE FOR LIST HERE
+	
+		
+		
+		add(logoutButton);
+		
+		
+		JRadioButton ratingOne = new JRadioButton("1");
+		ratingOne.setBounds(281, 452, 51, 37);
+		add(ratingOne);
+		
+		JRadioButton ratingTwo = new JRadioButton("2");
+		ratingTwo.setBounds(346, 452, 51, 37);
+		add(ratingTwo);
+		
+		JRadioButton ratingThree = new JRadioButton("3");
+		ratingThree.setBounds(411, 452, 51, 37);
+		add(ratingThree);
+		
+		JRadioButton ratingFour = new JRadioButton("4");
+		ratingFour.setBounds(476, 452, 51, 37);
+		add(ratingFour);
+		
+		JRadioButton ratingFive = new JRadioButton("5");
+		ratingFive.setBounds(541, 452, 51, 37);
+		add(ratingFive);
+		
+		
+		//ADDING BUTTONS TO GROUP SO WE ONLY CAN SELECT 1 OF THE 5
+		buttonGroup = new ButtonGroup();
+		
+		buttonGroup.add(ratingOne);
+		buttonGroup.add(ratingTwo);
+		buttonGroup.add(ratingThree);
+		buttonGroup.add(ratingFour);
+		buttonGroup.add(ratingFive);
+		
+		ButtonGroup mybutton = buttonGroup;
+		
+		JButton submitButton = new JButton("Submit");
+		submitButton.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				if(mybutton.isSelected(null))
+				{
+					submitButton.setEnabled(false);
+				}
+				else
+				{
+					//need to fix bug if i forget to select a rating it locks the submit button
+					
+					submitButton.setEnabled(true);
+					Enumeration<AbstractButton> bg = mybutton.getElements();
+					while(bg.hasMoreElements())
+					{
+					JRadioButton jrd = (JRadioButton) bg.nextElement();
+						if(jrd.isSelected())
+						{
+						      JOptionPane.showMessageDialog(null, "You Gave this picture a " + jrd.getText() + "/5");
+						}
+					}
+					int index = list.getSelectedIndex();
+				    if (index != -1) 
+				    {
+				        listModel.remove(index);	
+				    }
+				}
+				
+			}
+		});
+		
+		submitButton.setBounds(623, 452, 155, 37);
+		add(submitButton);
+		
+		JLabel lblIdNumber = new JLabel("ID Number");
+		lblIdNumber.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblIdNumber.setBackground(Color.WHITE);
+		lblIdNumber.setBounds(64, 87, 155, 29);
+		add(lblIdNumber);
+		
+		JPanel picturePanel = new JPanel();
+		picturePanel.setBounds(281, 111, 497, 318);
+		add(picturePanel);
 	}
 }
