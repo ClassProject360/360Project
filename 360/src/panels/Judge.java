7c1d7b391b9ef4c8662250dbf3ddc5cc32e578e8
ckubec <ckubec@gmail.com>
2016-03-18 12:02:02 -0700

    Spinner

--- 360/src/panels/Judge.java
+++ 360/src/panels/Judge.java
@@ -21,9 +21,10 @@ import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JSplitPane;
-import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import project.registerInfor;
@@ -42,7 +43,10 @@ public class Judge extends JPanel implements ListSelectionListener {
 	private registerInfor regis;
 	private JLabel userPictureLabel;
 	private JPanel scorePanel;
-	private JTextField scoreTextField;
+	
+	private JSpinner spinner;
+	private SpinnerNumberModel scoreSpinner;
+	
 	private JButton submitButton;
 	private String imgPath = "../360/submissions/ID_";
 	private StringBuilder data = new StringBuilder();
@@ -128,39 +132,31 @@ public class Judge extends JPanel implements ListSelectionListener {
 		
 		// display score information----------------------------------
 		scorePanel = new JPanel();
-		scoreTextField = new JTextField();
-		scoreTextField.setPreferredSize(new Dimension(100, 35));
-		scoreTextField.setFont(font1);
-		scorePanel.add(scoreTextField, BorderLayout.SOUTH);
+		
+		scoreSpinner = new SpinnerNumberModel(1,1,10,1);
+		spinner = new JSpinner(scoreSpinner);
+		
+		scorePanel.add(spinner, BorderLayout.SOUTH);
 			
 		submitButton = new JButton("Submit");
 
-		submitButton.addActionListener(new ActionListener() 
-		{
-			public void actionPerformed(ActionEvent arg0) 
-			{
-				if(!scoreTextField.getText().equals("")){
-					int tempNumb = scoreTextField.getText().charAt(0);
-					if(scoreTextField.getText().equals("") || tempNumb > 57 || tempNumb < 49){
-						JOptionPane.showMessageDialog (null,
-								 "Please select a score between 1 and 10",
-								 "Before Move on", JOptionPane.INFORMATION_MESSAGE);
-					}else{
-						int temp = data.indexOf("Score"+ idList.getSelectedValue().charAt(5)+ ": 0");
-						if(temp != -1){
-						data.replace(temp, temp+9, "Score" + idList.getSelectedValue().charAt(5) + ": " + scoreTextField.getText());
-						try {
-							regis.updateData(data.toString());
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-						scoreTextField.setText("");
-						} else
-							JOptionPane.showMessageDialog (null,
-									 "Apparently you have already graded this image, these aren't the droids you're looking for move along. "
-									 + "When done grading log out the next time logging in graded entries will be removed.",
-									 "Double Grading Move Along", JOptionPane.INFORMATION_MESSAGE);
+		submitButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0){			
+				int tempNumb = scoreSpinner.getNumber().intValue();
+				int temp = data.indexOf("Score"+ idList.getSelectedValue().charAt(5)+ ": 0");
+				
+				if(temp != -1){
+					data.replace(temp, temp+9, "Score" + idList.getSelectedValue().charAt(5) + ": " + tempNumb);
+					try {
+						regis.updateData(data.toString());
+					} catch (IOException e) {
+						e.printStackTrace();
 					}
+				} else {
+					JOptionPane.showMessageDialog (null,
+							 "Apparently you have already graded this image, these aren't the droids you're looking for move along. "
+							 + "When done grading log out the next time logging in graded entries will be removed.",
+							 "Double Grading Move Along", JOptionPane.INFORMATION_MESSAGE);
 				}
 			}
 		});
@@ -178,7 +174,6 @@ public class Judge extends JPanel implements ListSelectionListener {
         //Check if there is a data or not -------------------------------------------
         if (line == 0){
         	submitButton.setEnabled(false);
-        	scoreTextField.setEnabled(false);
         }else {
         	updateLabel(userId.elementAt(idList.getSelectedIndex()));
         }
